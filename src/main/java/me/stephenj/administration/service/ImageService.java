d8933b7965381d64e4e805c519f7f1563c23a257
StephenEvenson <2973114033@qq.com>
2019-10-13 21:15:43 +0800

    修改路径

--- src/main/java/me/stephenj/administration/service/ImageService.java
+++ src/main/java/me/stephenj/administration/service/ImageService.java
@@ -26,7 +26,7 @@ import java.util.Map;
 @Service
 public class ImageService {
     private final String PyTorch_REST_API_URL = "http://localhost:5000/hat_predict";
-    private final String UPLOADED_FOLDER = "/home/ralph/images";
+    private final String UPLOADED_FOLDER = "/home/ralph/images/";
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
     private final String HADOOP_ADDR = "hdfs://localhost:50070";
 
