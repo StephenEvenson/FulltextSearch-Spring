2f040e83bd7d2f05acabc40a6cc236c0c412e4b8
StephenEvenson <2973114033@qq.com>
2019-10-20 15:34:00 +0800

    解决跨域问题

--- src/main/java/me/stephenj/administration/controller/ImageController.java
+++ src/main/java/me/stephenj/administration/controller/ImageController.java
@@ -3,10 +3,7 @@ package me.stephenj.administration.controller;
 import me.stephenj.administration.model.Image;
 import me.stephenj.administration.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +14,7 @@ public class ImageController {
     @Autowired
     private ImageService imageService;
 
+    @CrossOrigin
     @RequestMapping(value = "/imgFile", method = RequestMethod.POST)
     public Image imageIdentifyFile(@RequestParam("file") MultipartFile file) {
 
