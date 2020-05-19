20a33514a16461d82670c7e10fe7b2764a9391fd
Stephen Evenson <1258455347@qq.com>
2020-05-19 19:48:25 +0800

    跨域

--- src/main/java/me/stephenj/administration/controller/UploadController.java
+++ src/main/java/me/stephenj/administration/controller/UploadController.java
@@ -3,12 +3,14 @@ package me.stephenj.administration.controller;
 import me.stephenj.administration.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/upload")
 public class UploadController {
 
