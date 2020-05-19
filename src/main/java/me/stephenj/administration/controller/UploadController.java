4f3653dba2019e673df30cb3779616b5da38eac1
Stephen Evenson <1258455347@qq.com>
2020-05-19 19:35:15 +0800

    添加文件上传

---
+++ src/main/java/me/stephenj/administration/controller/UploadController.java
@@ -0,0 +1,28 @@
+package me.stephenj.administration.controller;
+
+import me.stephenj.administration.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/upload")
+public class UploadController {
+
+    @Autowired
+    private UploadService uploadService;
+
+    @RequestMapping("file")
+    @ResponseBody
+    public int uploadFile(@RequestParam(value = "file", required = true)MultipartFile file) {
+        try {
+            return uploadService.uploadFile(file);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+}
---
+++ src/main/java/me/stephenj/administration/service/UploadService.java
@@ -0,0 +1,33 @@
+package me.stephenj.administration.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class UploadService {
+    private static String rootPath = "/home/stephen/solr/myfile";
+
+    public int uploadFile(MultipartFile file) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String fileDir = sdf.format(new Date());
+        File filePath = new File(rootPath + File.separator + fileDir + File.separator);
+        if (!filePath.exists()) {
+            filePath.mkdir();
+        }
+
+        String filename = file.getOriginalFilename();
+        String[] filenameArray = filename.split("\\.");
+        String prefix = filenameArray[filenameArray.length - 2];
+        String suffix = filenameArray[filenameArray.length - 1];
+        File newFile = new File(filePath, prefix + suffix);
+        for (int i = 1; newFile.exists() && i < Integer.MAX_VALUE; i++) {
+            newFile = new File(filePath, prefix + '(' + i + ')' + suffix);
+        }
+        file.transferTo(newFile);
+        return 1;
+    }
+}
